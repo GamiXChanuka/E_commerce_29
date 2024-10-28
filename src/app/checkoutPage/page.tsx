dc6831f20dbd6175c8b877e447dd56e65b56edb7
Gamith Chanuka <gamithchanuka123@gmail.com>
2024-10-29 01:15:19 +0530

    Refactor package.json to add required dependencies for dom-to-image, html2canvas, and jspdf libraries

--- src/app/checkoutPage/page.tsx
+++ src/app/checkoutPage/page.tsx
@@ -199,7 +199,10 @@ const CheckoutPage = () => {
 
           console.log("Order placed successfully:", response2.data);
           alert("Order placed successfully");
-
+          router.push({
+            pathname: '/thank-you',
+            query: { orderId: response2.data.OrderID, userId: 16 },
+          });      
           if (result.CartID) {
             setCartId(result.CartID);
           } else {
@@ -350,7 +353,7 @@ const CheckoutPage = () => {
                   id="radio_1"
                   type="radio"
                   name="radio"
-                  checked={deliveryMethod === "Fedex"}
+                  checked={deliveryMethod === "delivery"}
                   onChange={() => setDeliveryMethod("delivery")}
                 />
                 <span className="box-content absolute block w-4 h-4 -translate-y-1/2 bg-white border-8 border-gray-300 rounded-full peer-checked:border-blue-700 peer-checked:bg-gray-800 right-4 top-1/2"></span>
@@ -416,7 +419,7 @@ const CheckoutPage = () => {
                   type="radio"
                   name="radio"
                   defaultChecked
-                  checked={paymentMethod === "cod"}
+                  checked={paymentMethod === "COD"}
                   onChange={() => setPaymentMethod("COD")}
                 />
                 <span className="box-content absolute block w-4 h-4 -translate-y-1/2 bg-white border-8 border-gray-300 rounded-full peer-checked:border-blue-700 peer-checked:bg-gray-800 right-4 top-1/2"></span>
@@ -441,8 +444,8 @@ const CheckoutPage = () => {
                   id="radio_4"
                   type="radio"
                   name="radio"
-                  checked={paymentMethod === "card"}
-                  onChange={() => setPaymentMethod("COD")}
+                  checked={paymentMethod === "Card"}
+                  onChange={() => setPaymentMethod("Card")}
                 />
                 <span className="box-content absolute block w-4 h-4 -translate-y-1/2 bg-white border-8 border-gray-300 rounded-full peer-checked:border-blue-700 peer-checked:bg-gray-800 right-4 top-1/2"></span>
                 <label
@@ -626,7 +629,7 @@ const CheckoutPage = () => {
 
           <p className="text-xl font-medium text-black">Payment Details</p>
 
-          {paymentMethod === "card" && (
+          {paymentMethod === "Card" && (
             <div>
               {/* Cardholder Name */}
               <label
--- src/app/thank-you/page.tsx
+++ src/app/thank-you/page.tsx
@@ -1,46 +1,126 @@
-import React from 'react';
+"use client";
+import React, { useEffect, useRef, useState } from "react";
+import { useRouter } from "next/navigation";
+import axios from "axios";
+import Spinner from "@/components/spinner/Spinner";
+import { ArrowLeftStartOnRectangleIcon } from "@heroicons/react/24/outline";
+import domtoimage from "dom-to-image";
+import { jsPDF } from "jspdf";
+
+interface OrderItem {
+  VariantName: string;
+  Price: string;
+  Quantity: number;
+  ImageLink: string;
+}
+
+interface OrderDetails {
+  OrderDate: string;
+  DeliveryType: string;
+  PaymentMethod: string;
+  OrderTotal: string;
+  items: OrderItem[];
+}
 
 const Invoice = () => {
+  const router = useRouter();
+  const orderId = 16; // hardcoded order ID
+  const [orderDetails, setOrderDetails] = useState<OrderDetails | null>(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
+
+  const invoiceRef = useRef(null);
+
+  const downloadPdf = () => {
+    const node = invoiceRef.current;
+    if (!node) return;
+
+    domtoimage.toPng(node, { quality: 1, bgcolor: '#fff', })
+      .then((dataUrl) => {
+        const pdf = new jsPDF("p", "mm", "a4");
+        const imgProps = pdf.getImageProperties(dataUrl);
+        const pdfWidth = pdf.internal.pageSize.getWidth();
+        const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
+
+        pdf.addImage(dataUrl, "PNG", 0, 0, pdfWidth, pdfHeight);
+        pdf.save("invoice.pdf");
+      })
+      .catch((error) => {
+        console.error("Error generating PDF", error);
+      });
+  };
+
+  const fetchOrderDetails = async () => {
+    try {
+      const response = await axios.post("/api/getOrderDetails", {
+        orderId,
+      });
+      setOrderDetails(response.data);
+    } catch (err: any) {
+      setError(err.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchOrderDetails();
+  }, []);
+
+  if (loading) {
+    return <Spinner />;
+  }
+
+  if (error) {
+    return <p>Error: {error}</p>;
+  }
+
   return (
     <section className="py-20 bg-gray-100">
-      <div className="max-w-2xl py-0 mx-auto md:py-16">
-        <article className="overflow-hidden shadow-none md:shadow-md md:rounded-md">
-          <div className="bg-white md:rounded-b-md">
+
+      {/* Banner Section */}
+      <div className="relative z-10 flex items-center justify-center py-4 font-semibold text-center text-white bg-green-500 shadow-lg px-9 md:max-w-2xl md:mx-auto md:rounded-t-md">
+        <p>Thank you for purchasing! Your delivery will arrive within the next 3 days.</p>
+      </div>
+ 
+      <div className="max-w-2xl py-0 mx-auto md:py-16 " >
+        <article className="overflow-hidden shadow-none md:shadow-md md:rounded-md invoice-content"  ref={invoiceRef}>
+          <div className="bg-white md:rounded-b-md" >
             <div className="border-b border-gray-200 p-9">
               <div className="space-y-6">
                 <div className="flex justify-between items-top">
                   <div className="space-y-4">
                     <div>
-                      <p className="text-2xl font-bold text-black text-blue-900">Shopzy</p>
-
+                      <p className="text-2xl font-bold text-blue-900">Shopzy</p>
                       <p className="text-lg font-bold text-black">Invoice</p>
-                      {/* <p>Shopzy</p> */}
                     </div>
                     <div>
                       <p className="text-sm font-medium text-gray-400">Billed To</p>
-                      <p className='text-black'>Tony Stark</p>
-                      <p className='text-black'>tony@starkindustriesxyz.com</p>
-                      <p className='text-black'>(02) 1234 1234</p>
+                      <p className="text-black">Tony Stark</p>
+                      <p className="text-black">tony@starkindustriesxyz.com</p>
+                      <p className="text-black">(02) 1234 1234</p>
                     </div>
                   </div>
                   <div className="space-y-2">
                     <div>
                       <p className="text-sm font-medium text-gray-400">Invoice Number</p>
-                      <p className='text-black'>INV-MJ0001</p>
+                      <p className="text-black">INV-MJ0001</p>
                     </div>
                     <div>
                       <p className="text-sm font-medium text-gray-400">Invoice Date</p>
-                      <p className='text-black'>31 December 2021</p>
+                      <p className="text-black">{new Date(orderDetails?.OrderDate ?? "").toLocaleDateString()}</p>
                     </div>
                     <div>
                       <p className="text-sm font-medium text-gray-400">ABN</p>
-                      <p className='text-black'>57 630 182 446</p>
+                      <p className="text-black">57 630 182 446</p>
                     </div>
                     <div>
                       <a
                         href="#"
                         target="_blank"
-                        className="inline-flex items-center text-sm font-medium text-blue-500 hover:opacity-75"
+                        className="inline-flex items-center text-sm font-medium text-blue-500 hover:opacity-75 btn-download"
+                        onClick={downloadPdf} 
                       >
                         Download PDF
                         <svg
@@ -55,25 +135,6 @@ const Invoice = () => {
                         </svg>
                       </a>
                     </div>
-                    {/* <div>
-                      <a
-                        href="#"
-                        target="_blank"
-                        className="inline-flex items-center text-sm font-medium text-blue-500 hover:opacity-75"
-                      >
-                        Pay Balance
-                        <svg
-                          className="ml-0.5 h-4 w-4 fill-current"
-                          xmlns="http://www.w3.org/2000/svg"
-                          viewBox="0 0 20 20"
-                          fill="currentColor"
-                          aria-hidden="true"
-                        >
-                          <path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z" />
-                          <path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z" />
-                        </svg>
-                      </a>
-                    </div> */}
                   </div>
                 </div>
               </div>
@@ -87,41 +148,44 @@ const Invoice = () => {
                 <tr>
                   <th className="py-4 font-semibold text-left text-gray-400 px-9">Item</th>
                   <th className="py-3 font-semibold text-left text-gray-400"></th>
-                  <th className="py-3 font-semibold text-left text-gray-400">Amount</th>
-                  <th className="py-3 font-semibold text-left text-gray-400">Discount</th>
+                  <th className="py-3 font-semibold text-center text-gray-400">Quantity    </th>
+                  
+                  <th className="py-3 ml-5 font-semibold text-left text-gray-400">       Amount</th>
                   <th className="py-3 font-semibold text-left text-gray-400"></th>
                 </tr>
               </thead>
-              <tbody className="divide-y divide-gray-200">
-                <tr>
-                  <td className="flex items-center py-5 space-x-1 px-9 whitespace-nowrap">
-                    <div>
-                      <p className='text-black'>Jericho III (YA-4)</p>
-                      <p className="text-sm text-gray-400">Nuclear-armed ICBM</p>
-                    </div>
-                  </td>
-                  <td className="text-gray-600 truncate whitespace-nowrap"></td>
-                  <td className="text-gray-600 truncate whitespace-nowrap">$380,000.00</td>
-                  <td className="text-gray-600 truncate whitespace-nowrap">0%</td>
-                </tr>
-                <tr>
-                  <td className="flex items-center py-5 space-x-1 px-9 whitespace-nowrap">
-                    <div>
-                      <p className='text-black'>Pym Particles (Pack of 10,000)</p>
-                      <p className="text-sm text-gray-400">Redacted Description</p>
-                    </div>
-                  </td>
-                  <td className="text-gray-600 truncate whitespace-nowrap"></td>
-                  <td className="text-gray-600 truncate whitespace-nowrap">$280,000.00</td>
-                  <td className="text-gray-600 truncate whitespace-nowrap">0%</td>
-                </tr>
-              </tbody>
+
+
+                <tbody className="divide-y divide-gray-200">
+                {orderDetails?.items.map((item, index) => (
+                    <tr key={index}>
+                    <td className="flex items-center py-5 space-x-1 px-9 whitespace-nowrap">
+                        <img
+                        src={item.ImageLink}
+                        alt={item.VariantName}
+                        className="object-cover w-16 h-16 mr-4 rounded-lg"
+                        />
+                        <div>
+                        <p className="font-bold text-black">{item.VariantName}</p>
+                        <p className="text-sm text-gray-400">Description or type of item</p>
+                        </div>
+                    </td>
+                    <td className="text-gray-600 truncate whitespace-nowrap"> {item.Quantity}</td>
+                    <td className="text-gray-600 truncate whitespace-nowrap">             </td>
+
+                    <td className="text-gray-600 truncate whitespace-nowrap">${item.Price}</td>
+                    </tr>
+                ))}
+                </tbody>
+
+
+
             </table>
             <div className="border-b border-gray-200 p-9">
               <div className="space-y-3">
                 <div className="flex justify-between">
                   <p className="text-sm text-gray-500">Subtotal</p>
-                  <p className="text-sm text-gray-500">$660,000.00</p>
+                  <p className="text-sm text-gray-500">{orderDetails?.OrderTotal}</p>
                 </div>
                 <div className="flex justify-between">
                   <p className="text-sm text-gray-500">Tax</p>
@@ -129,7 +193,7 @@ const Invoice = () => {
                 </div>
                 <div className="flex justify-between">
                   <p className="text-sm text-gray-500">Total</p>
-                  <p className="text-sm text-gray-500">$660,000.00</p>
+                  <p className="text-sm text-gray-500">{orderDetails?.OrderTotal}</p>
                 </div>
               </div>
             </div>
@@ -137,7 +201,7 @@ const Invoice = () => {
               <div className="space-y-3">
                 <div className="flex justify-between">
                   <p className="text-lg font-bold text-black">Amount Due</p>
-                  <p className="text-lg font-bold text-black">$360.00</p>
+                  <p className="text-lg font-bold text-black">{orderDetails?.OrderTotal}</p>
                 </div>
               </div>
             </div>
